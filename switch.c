8808c9cae16d73bcfdf3128fc0175cbd6adeef77
zeimedee <42755623+zeimedee@users.noreply.github.com>
2022-09-15 15:24:52 +0000

    c programming revision switch

---
+++ switch.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void){
+
+    switch(4){
+        case 1: 
+        printf("case 1");
+        break;
+
+        case 2:
+        printf("case 2");
+        break;
+
+        default:
+        printf("default");
+    }
+    return 0;
+}
